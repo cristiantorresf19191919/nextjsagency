45c66a6db64e654325734013529e27ec5738a005
Cristian Torres <cristian.torres@techdata.com>
2022-02-14 12:59:03 -0500

    first commit

---
+++ src/components/Freelancre-intro/freelancre-intro.jsx
@@ -0,0 +1,96 @@
+/* eslint-disable @next/next/no-img-element */
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+const FreelancreIntro = () => {
+  return (
+    <header className="freelancre valign">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-4">
+            <div className="img">
+              <img src="/img/KAREEEEEEEEEEEEEEN.png" alt="" />
+            </div>
+          </div>
+          <div className="col-lg-8 valign">
+            <div className="cont">
+              <h1 className="cd-headline clip">
+                Hola, mi nombre es karen y soy la novia de cristianScript, soy ux senior designer, desarrollo 
+                <span
+                  style={{ fontSize: "35px", lineHeight: "49px" }}
+                  className="cd-words-wrapper"
+                >
+                  <Typewriter
+                    options={{
+                      wrapperClassName: "color-font fw-600",
+                      strings: [
+                        "Aplicaciones Mobiles",
+                        "Landing Pages",
+                        "Diseños Espectaculares",
+                      ],
+                      autoStart: true,
+                      loop: true,
+                    }}
+                    loop={true}
+                    onInit={(typewriter) => {
+                      typewriter;
+                    }}
+                  />
+                </span>
+              </h1>
+            </div>
+          </div>
+        </div>
+
+        <div className="states">
+          <div className="container">
+            <ul className="flex">
+              <li className="flex">
+                <div className="numb valign">
+                  <h3>12</h3>
+                </div>
+                <div className="text valign">
+                  <p>
+                    Años <br /> de experience
+                  </p>
+                </div>
+              </li>
+
+              <li className="flex">
+                <div className="numb valign">
+                  <h3>165</h3>
+                </div>
+                <div className="text valign">
+                  <p>
+                    Proyectos Completados <br /> en 19 paises
+                  </p>
+                </div>
+              </li>
+
+              <li className="mail-us">
+                <a href="mailto:your@email.com?subject=Subject">
+                  <div className="flex">
+                    <div className="text valign">
+                      <div className="full-width">
+                        <p>Ponte en contacto conmigo</p>
+                        <h6>netgoucreativoux@gmail.com</h6>
+                      </div>
+                    </div>
+                    <div className="mail-icon">
+                      <div className="icon-box">
+                        <span className="icon color-font pe-7s-mail"></span>
+                      </div>
+                    </div>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div className="line bottom left"></div>
+    </header>
+  );
+};
+
+export default FreelancreIntro;
